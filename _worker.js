a8db4638dfe159d60dbb038d8e6e31e75dda907f
baltictradingclub8-create <baltictradingclub8@gmail.com>
2026-05-09 21:06:02 -0700

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,16 @@
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    if (url.pathname === '/api/robbery-state') {
+      const res = await fetch('https://api.jbvalues.com/v1/robbery-state');
+      const data = await res.json();
+      return new Response(JSON.stringify(data), {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      });
+    }
+    return fetch(request);
+  }
+}
